ec7c90a73357e5deb0f08c6f2d9295c9d554ce1a
koopolina <koopolina@gmail.com>
2021-10-05 17:00:21 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-useless-concat */
+/* eslint-disable no-shadow */
+/* eslint-disable id-length */
+/* eslint-disable no-unused-vars */
 function getRandomInt(min, max) {
   if(min >= max) {
     throw 'До не может быть меньше или равно значению от';
@@ -7,14 +11,62 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-getRandomInt(0, 55);
+// const lengthRange = (string, maxLength) => {
+//   const userInput = string;
+//   return userInput <= maxLength;
+// };
 
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// lengthRange(135, 140);
 
+const names = [
+  'Алиса',
+  'Женя',
+  'Игорь',
+  'Мила',
+  'Аня',
+  'Арнольд',
+];
 
-const lengthRange = (inputtxt, maxLength) => {
-  const userInput = inputtxt;
-  return userInput <= maxLength;
+const text = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+];
+
+const count = 25;
+
+const createComments = () => {
+  const randomIdIndex = getRandomInt(1, 25);
+  const number = getRandomInt(1, 6);
+  const randomTextIndex = getRandomInt(0, text.length);
+  const randomNameIndex = getRandomInt(0, names.length -1);
+
+  return {
+    id: randomIdIndex,
+    avatar: `img/avatar-${  number  }.` + 'svg',
+    message: text[randomTextIndex],
+    name: names[randomNameIndex],
+  };
 };
 
-lengthRange(135, 140);
+// console.log(createComments());
+
+const createPost = () => {
+  const randomIdIndex = getRandomInt(1, 25);
+  const i = getRandomInt(1, 25);
+  const randomLikesIndex = getRandomInt(15, 200);
+  const createComments = getRandomInt(1, 5);
+
+  return {
+    id: randomIdIndex,
+    url: `photos/${  i  }.jpg`,
+    description: 'Всем привет! Как дела?',
+    likes: randomLikesIndex,
+    comments: createComments,
+  };
+};
+
+const similarPost = Array.from({length: count}, createPost);
+
+// console.log(createPost());
+
+// console.log(similarPost);
