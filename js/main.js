ecc3e21130fe6619b2110ac23b4f4d81c568d401
koopolina <koopolina@gmail.com>
2021-10-09 15:33:55 +0300

    Внесла правки

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-import './data.js';
-import {NAMES, POST_MESSAGES, NUMBER_MESSAGES, createComments, createPost, postList, similarPosts} from './data.js';
+import '/data.js';
+import {NAMES, POST_MESSAGES, NUMBER_MESSAGES, createComments, createPost, postList, similarPosts} from '/data.js';
